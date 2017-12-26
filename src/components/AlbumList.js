fe9b935b33b703318fe95304ecc73eccd2677cd0
Ezz Khayyat <ekhayyat@gmail.com>
2017-12-26 22:37:08 +0200

    Initial commit

---
+++ src/components/AlbumList.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
+
+class AlbumList extends Component {
+    state = { albums: [] };
+
+    componentWillMount() {
+        axios.get('http://rallycoding.herokuapp.com/api/music_albums')
+        .then((res) => {
+            this.setState({
+                albums: res.data
+            });
+        });
+    }
+
+    renderAlbums() {
+        return this.state.albums.map(album => this.renderAlbum(album));
+    }
+
+    renderAlbum(album) {
+        return (
+            <AlbumDetail key={album.title} album={album} />
+        );
+    }
+
+    render() {
+        return (
+            <View>
+                {this.renderAlbums()}
+            </View>
+        );
+    }
+}
+
+export default AlbumList;
